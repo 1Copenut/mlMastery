e95fbe15fd9eb80f98fee74e230240b1351a55e9
Trevor Pierce <trevor.pierce@continuumdesign.net>
2016-05-14 17:47:59 -0500

    Adding methods for summarizing, and pulling out mean and stdev.

--- 2014-12-08/naive_bayes.py
+++ 2014-12-08/naive_bayes.py
@@ -2,6 +2,8 @@
 
 import csv
 import random
+import math
+
 
 def loadCsv(filename):
     lines = csv.reader(open(filename, 'rb'))
@@ -10,6 +12,7 @@ def loadCsv(filename):
     for i in range(len(dataset)):
         dataset[i] = [float(x) for x in dataset[i]]
     return dataset
+
 
 def splitDataset(dataset, splitRatio):
     trainSize = int(len(dataset) * splitRatio)
@@ -20,13 +23,35 @@ def splitDataset(dataset, splitRatio):
         index = random.randrange(len(copy))
         trainSet.append(copy.pop(index))
     return [trainSet, copy]
-"""
-filename = 'pima-indians-diabetes.csv'
-dataset = loadCsv(filename)
-print('Loaded data file {0} with {1} rows').format(filename, len(dataset))
 
-dataset = [[1], [2], [3], [4], [5]]
-splitRatio = 0.67
-train, test = splitDataset(dataset, splitRatio)
-print('Split {0} rows into train with {1} and test with {2}').format(len(dataset), train, test)
-"""
+
+def separateByClass(dataset):
+    separated = {}
+
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        
+        if (vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
+
+
+def mean(numbers):
+    return sum(numbers)/float(len(numbers))
+
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x-avg, 2) for x in numbers])/float(len(numbers) -1)
+    return math.sqrt(variance)
+
+
+def summarize(dataset):
+    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+    del summaries[-1]
+    return summaries
+
+
+dataset = [[1, 20, 0], [2, 21, 1], [3, 22, 0]]
+summary = summarize(dataset)
+print('Attribute summaries: {0}').format(summary)
